410f3d3006eb4cde0ff8691c4548f6dbc49a6d8a
Nitin <nitin.manhas@pw.live>
2024-07-15 11:45:45 +0530

    Initial Commit

---
+++ SpmPoc2/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  SpmPoc2
+//
+//  Created by PW on 08/07/24.
+//
+
+import SwiftUI
+import swiftpackage
+
+struct ContentView: View {
+    var body: some View {
+        MyCustomView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpmPoc2/SpmPoc2App.swift
@@ -0,0 +1,17 @@
+//
+//  SpmPoc2App.swift
+//  SpmPoc2
+//
+//  Created by PW on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SpmPoc2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
